b0f54878a85c904bba538fe58d0f073981ac5539
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2022-12-29 14:46:33 +0400

    footer developer add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,6 +14,7 @@ function Footer() {
     <footer>
       <div className="container">
         <span>© 2022, Bütün hüquqlar qorunur.</span>
+        <a id="developer" href="https://www.linkedin.com/in/ravan-khankishiyev-75a685223">Website developing by <strong>Ravan Khankishiyev</strong></a>
         <div id="social-media-icons">
           <button
             onClick={
